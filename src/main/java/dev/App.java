42098212d2b4517432d3a7d2cc58d2fbc0fed099
Guillaume Pierre <guillaumepierre56@gmail.com>
2019-04-12 15:01:36 +0200

    ajout tp 2

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -1,5 +1,7 @@
 package dev;
 
+import com.github.lalyos.jfiglet.FigletFont;
+
 /**
  * Hello world!
  *
@@ -9,5 +11,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Ello World!" );
+        String a = FigletFont.convertOneLine("Guillaume");
+        System.out.println(a);
     }
 }
